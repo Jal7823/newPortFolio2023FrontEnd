a726a6d6f708c72061f5d3be578baf4755d0f5d8
jal7823 <Jal7823@gmail.com>
2023-10-14 17:53:54 -0300

    feat: :heavy_plus_sign: Show actions bottoms
    Show actions bottoms

--- src/pages/portfolio/index.js
+++ src/pages/portfolio/index.js
@@ -3,7 +3,7 @@ import "./style.css";
 import { Helmet, HelmetProvider } from "react-helmet-async";
 import { Container, Row, Col } from "react-bootstrap";
 import { meta } from "../../content_option";
-import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { FaGithub, FaChrome,FaInfoCircle } from "react-icons/fa";
 
 export const Portfolio = () => {
   const [Projects, setProjects] = useState([]);
@@ -37,41 +37,39 @@ export const Portfolio = () => {
             return (
               <div>
                 <div key={i} className="po_item">
-                <a target="_blank"  rel="noreferrer" href={data.url}>
-                  <img src={data.image} alt="" />
-                  <div className="content">
-                    <p>{data.description}</p>
-                    
-                      view project
-                    
-                  </div>
+                  <a target="_blank" rel="noreferrer" href={data.url}>
+                    <img src={data.image} alt="" />
+                    <div className="content">
+                      <p>{data.description}</p>
+                    </div>
                   </a>
                 </div>
                 <div className="info">
                   <h4>{data.name}</h4>
                 </div>
-                
-
                 <hr />
                 <div>
-                  <h5 className="text-center">Stack</h5>
                   {data.stack.map((el, i) => (
                     <ul>
-                      <li><span key={i}>- {el.name}</span></li>
+                      <li>
+                        <span key={i}>- {el.name}</span></li>
                     </ul>
-                    
-                    ))}
+                  ))}
                 </div>
-                <br />
-                <hr />
-
-                <div className="icons_flow">
-                  <a target="_blank"  rel="noreferrer" href={data.github}>
-                    <FaGithub />
-                  </a>
-                  <a target="_blank"  rel="noreferrer" href={data.url}>
-                    <FaLinkedin />
-                  </a>
+                <div className="footer-stack mb-4">
+                  <div className="icons_flow">
+                    <a target="_blank" rel="noreferrer" href={data.github}>
+                      <FaGithub />
+                    </a>
+                    <a
+                      className=""
+                      target="_blank"
+                      rel="noreferrer"
+                      href={data.url}
+                    >
+                      <FaChrome />
+                    </a>
+                  </div>
                 </div>
               </div>
             );
